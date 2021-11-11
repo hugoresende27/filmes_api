ad98a687290b476bfb8ccfcaae2917835b8d66e9
hugoresende27 <Hugoresende27@gmail.com>
2021-11-11 23:05:47 +0000

    q

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280' //witdth 1280 sem /
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=43031326262e3f481d9e4f999a63f52b&query="'//query=" para concatenar o que está na search box com este url e apresentar resultado
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 //GET INICIAL MOVIES
@@ -20,7 +21,48 @@ async function getMovies (url) {
     const data = await res.json()
 
     console.log(data.results) //console log de um array de 20 objetos, o top de filmes
+    showMovies(data.results)
 }
+
+function showMovies(movies) {
+    main.innerHTML =''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average,overview} = movie
+    
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+    movieEl.innerHTML= `
+        
+            <img src="${IMG_PATH + poster_path}"alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+              ${overview}
+            </div>
+    `
+
+    main.appendChild(movieEl)
+})
+}
+
+
+
+function getClassByRate(voto) {
+    if (voto >= 8) {
+        return 'green'
+    } else if (voto >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+    }
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()//como é submit, para não submit a página
